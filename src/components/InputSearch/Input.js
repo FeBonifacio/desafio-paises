ee82731cb4d825a47effac5c473b904fba9622e9
Fernando <fe.bonifacio@hotmail.com>
2024-03-07 13:50:11 -0300

    'Historico finalizado'

--- src/components/InputSearch/Input.js
+++ src/components/InputSearch/Input.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './styles.css';
 import Table from '../TableS';
 
 const Input = () => {
@@ -6,22 +7,34 @@ const Input = () => {
     const [searchResult, setSearchResult] = useState([]);
     const [searchClick, setSearchClick] = useState(false); // Vai aparecer somente quando clicar no botão
     const [searchHistory, setSearchHistory] = useState([]);
-    
+
+    //useEffect para recuperar o histórico de consultas 
+    useEffect(() => {
+        const storedHistory = localStorage.getItem('searchHistory');
+        if (storedHistory) {
+            setSearchHistory(JSON.parse(storedHistory));
+        }
+    }, []);
 
     const handleInput = (e) => {
         setNameCountries(e.target.value);
     }
 
+    //Submit normal
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         setSearchClick(true); // Define como verdadeiro quando clicado
 
         try {
-            // Buscar país
-            const response = await fetch(`https://restcountries.com/v3.1/name/${nameCountries}`);
-            const data = await response.json();
-            setSearchResult(data);
+        // Buscar país
+        const response = await fetch(`https://restcountries.com/v3.1/name/${nameCountries}`);
+        const data = await response.json();
+        setSearchResult(data);
+
+        // para salvar no localS
+        const updatedHistory = [...searchHistory, nameCountries];
+        localStorage.setItem('searchHisory', JSON.stringify(updatedHistory));
         } catch (error) {
             console.error('Erro ao buscar país:', error);
         }
@@ -29,12 +42,13 @@ const Input = () => {
         setSearchHistory(preventHistory => [...preventHistory, nameCountries]); // colocar tudo no array
     }
 
+    // Historicos da tabela
     const handleHistory = async (countryName) => {
         setNameCountries(countryName);
         setSearchClick(true);
 
         try {
-            // Buscar país
+            // Tenho q buscar a api de novo
             const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
             const data = await response.json();
             setSearchResult(data);
@@ -44,23 +58,28 @@ const Input = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="query"
-                    value={nameCountries}
-                    onChange={handleInput}
-                    placeholder='Brasil, Argentina...'
-                />
-                <button type="submit">Buscar</button>
-            </form>
+        <div className='container-all'>
+            <div className='div-input'>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="query"
+                        value={nameCountries}
+                        onChange={handleInput}
+                        placeholder='Brasil, Argentina...'
+                    />
+
+                    <button className='btn' type="submit">Buscar</button>
+                </form>
+            </div>
+            
 
             {searchClick && (
-                <ul>
+                <div className='contant-table'>
+                    <ul>
                     {searchResult.length > 0 ? (
                         searchResult.map(country => (
-                            <li key={country.name?.common}>
+                            <li className="itens" key={country.name?.common}>
                                 <h1>{country.name?.common}</h1>
                                 <p>Capital: {country.capital}</p>
                                 <p>População: {country.population}</p>
@@ -71,7 +90,8 @@ const Input = () => {
                     ) : (
                         <p>Nenhum resultado encontrado.</p>
                     )}
-                </ul>
+                    </ul>
+                </div>
             )}
 
         <Table searchHistory={searchHistory} handleHistory={handleHistory} />
