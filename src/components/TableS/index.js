39b07533d951ef2c315a5ddb97c19221b9f02a50
Fernando <fe.bonifacio@hotmail.com>
2024-03-07 15:14:49 -0300

    'Criei o csv'

--- src/components/TableS/index.js
+++ src/components/TableS/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './styles.css';
 
 const Table = ({ searchHistory, handleHistory }) => {
+
+    //https://www.youtube.com/watch?v=YJCZCdLEMo4
+    //https://www.npmjs.com/package/csv-parser
+    //CHATGTP
+    // Função para converter a tabela em formato CSV
+    const convertToCSV = () => {
+        const csvContet = "data:text/csv;charset=utf-8,"
+        + searchHistory.map(row => row).join("\n");
+    return encodeURI(csvContet);
+    }
+
+    //Função de download
+    const downloadCSV = () => {
+        const csvData = convertToCSV();
+        const link = document.createElement("a");
+        link.setAttribute('href', csvData);
+        link.setAttribute('download', 'historico.csv');
+        document.body.appendChild(link);
+        link.click();
+    }
+
     return (
+    <>
         <div className='content-table'>
             <table>
                 <thead>
@@ -21,6 +43,12 @@ const Table = ({ searchHistory, handleHistory }) => {
                 </tbody>
             </table>
         </div>
+        
+        <div className='buttonCSV'>
+            <button onClick={downloadCSV}>Exporta arquivo</button>
+        </div>
+    </>
+        
     );
 }
 
